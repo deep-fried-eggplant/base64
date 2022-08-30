dccdc1aafa8755820a076d2a11272049b896b836
yoshi1378 <yoshi1378.pp.tt@gmail.com>
2022-08-30 13:45:01 +0900

    add include

---
+++ include/base64.hpp
@@ -0,0 +1,263 @@
+#ifndef DFE_BASE64_HPP
+#define DFE_BASE64_HPP
+
+#include<string>
+#include<vector>
+#include<stdexcept>
+
+/**
+ * @namespace dfe
+ * @brief deep-fried-eggplant.com
+*/
+namespace dfe{
+    /**
+     * @namespace base64
+     * @brief namespace for base64 utils
+    */
+    namespace base64{
+        
+        /**
+         * @fn encode
+         * @param binaryData std::vector<T> which sizeof T is 1 byte and contains binary data
+         * @return base64 encoded string
+        */
+        template<typename T,typename std::enable_if<sizeof(T)==1,std::nullptr_t>::type=nullptr>
+        std::string encode(const std::vector<T> &binaryData);
+        
+        /**
+         * @fn encode
+         * @param binaryData std::string which contains binary data
+         * @return base64 encoded string
+        */
+        std::string encode(const std::string &binaryData);
+        
+        /**
+         * @fn decode
+         * @param base64str base64 encoded string
+         * @return binary data as std::vector<T> which sizeof T is 1 byte
+        */
+        template<typename T,typename std::enable_if<sizeof(T)==1,std::nullptr_t>::type=nullptr>
+        std::vector<T> decode(const std::string &base64str);
+        
+        /**
+         * @fn decode
+         * @param base64str base64 encoded string
+         * @return binary data as std::string
+        */
+        std::string decode(const std::string &base64str);
+
+    }
+    /**
+     * @namespace detail
+     * @brief detail implement
+    */
+    namespace detail{
+        char byteToBase64char(const uint8_t b);
+        uint8_t base64charToByte(const char c);
+    }
+
+    namespace exception{
+        class Base64Exception : public std::runtime_error{
+            public:
+            Base64Exception(const char* message):runtime_error(message){}
+        };
+    }
+
+    namespace base64{
+        using Exception=exception::Base64Exception;
+
+        template<typename T,typename std::enable_if<sizeof(T)==1,std::nullptr_t>::type=nullptr>
+        std::string encode(const std::vector<T> &binaryData){
+            std::string res;
+            uint8_t phase=0;
+            uint8_t b=0;
+            for(T byt : binaryData){
+                char ch=char(byt);
+                switch(phase){
+                    case 0:
+                    {
+                        b = ch>>2 & 0b111111;
+                        res+=detail::byteToBase64char(b);
+                        b = (ch&0b11)<<4;
+                    } break;
+                    case 1:
+                    {
+                        b += ch>>4 & 0b1111;
+                        res+=detail::byteToBase64char(b);
+                        b = (ch&0b1111)<<2;
+                    } break;
+                    case 2:
+                    {
+                        b += ch>>6 & 0b11;
+                        res+=detail::byteToBase64char(b);
+                        b = ch&0b111111;
+                        res+=detail::byteToBase64char(b);
+                        b = 0;
+                    } break;
+                }
+                phase=(phase+1)%3;
+            }
+            if(phase>0){
+                res+=detail::byteToBase64char(b);
+            }
+            if(res.length()%4>0){
+                for(int i=res.length()%4; i<4; i++){
+                    res+='=';
+                }
+            }
+            return res;
+        }
+        inline std::string encode(const std::string &binaryData){
+            std::string res;
+            uint8_t phase=0;
+            uint8_t b=0;
+            for(auto ch : binaryData){
+                switch(phase){
+                    case 0:
+                    {
+                        b = ch>>2 & 0b111111;
+                        res+=detail::byteToBase64char(b);
+                        b = (ch&0b11)<<4;
+                    } break;
+                    case 1:
+                    {
+                        b += ch>>4 & 0b1111;
+                        res+=detail::byteToBase64char(b);
+                        b = (ch&0b1111)<<2;
+                    } break;
+                    case 2:
+                    {
+                        b += ch>>6 & 0b11;
+                        res+=detail::byteToBase64char(b);
+                        b = ch&0b111111;
+                        res+=detail::byteToBase64char(b);
+                        b = 0;
+                    } break;
+                }
+                phase=(phase+1)%3;
+            }
+            if(phase>0){
+                res+=detail::byteToBase64char(b);
+            }
+            if(res.length()%4>0){
+                for(int i=res.length()%4; i<4; i++){
+                    res+='=';
+                }
+            }
+            return res;
+        }
+        
+        
+        template<typename T,typename std::enable_if<sizeof(T)==1,std::nullptr_t>::type=nullptr>
+        std::vector<T> decode(const std::string &base64str){
+            std::vector<T> res;
+            uint8_t phase=0;
+            char b=0;
+            for(char ch : base64str){
+                uint8_t tmp=detail::base64charToByte(ch);
+                if(tmp==64){
+                    if(phase==1){
+                        res.push_back((T)b);
+                    }
+                    break;
+                }else if(tmp==0xff){
+                    throw Exception("invalid character in base64 string");
+                }
+
+                switch(phase){
+                    case 0:
+                    {
+                        b = tmp<<2;
+                    }break;
+                    case 1:
+                    {
+                        b += tmp>>4 & 0b11;
+                        res.push_back((T)b);
+                        b = (tmp&0b1111)<<4;
+                    }break;
+                    case 2:
+                    {
+                        b += tmp>>2 & 0b1111;
+                        res.push_back((T)b);
+                        b = (tmp&0b11)<<6;
+                    }break;
+                    case 3:
+                    {
+                        b += tmp & 0b111111;
+                        res.push_back((T)b);
+                    }break;
+                }
+                phase=(phase+1)%4;
+            }
+            return res;
+        }
+        std::string decode(const std::string &base64str){
+            std::string res;
+            uint8_t phase=0;
+            char b=0;
+            for(char ch : base64str){
+                uint8_t tmp=detail::base64charToByte(ch);
+                if(tmp==64){
+                    if(phase==1){
+                        res+=b;
+                    }
+                    break;
+                }else if(tmp==0xff){
+                    throw Exception("invalid character in base64 string");
+                }
+                
+                switch(phase){
+                    case 0:
+                    {
+                        b = tmp<<2;
+                    }break;
+                    case 1:
+                    {
+                        b += tmp>>4 & 0b11;
+                        res += b;
+                        b = (tmp&0b1111)<<4;
+                    }break;
+                    case 2:
+                    {
+                        b += tmp>>2 & 0b1111;
+                        res += b;
+                        b = (tmp&0b11)<<6;
+                    }break;
+                    case 3:
+                    {
+                        b += tmp & 0b111111;
+                        res += b;
+                    }break;
+                }
+                phase=(phase+1)%4;
+            }
+            return res;
+        }
+
+    }
+
+    namespace detail{
+        inline char byteToBase64char(const uint8_t b){
+            return
+                (b<26)?('A'+b)
+                :(b<52)?('a'+(b-26))
+                :(b<62)?('0'+(b-52))
+                :(b==62)?'+'
+                :(b==63)?'/'
+                :0;
+        }
+        
+        inline uint8_t base64charToByte(const char c){
+            return
+                (c>='A'&&c<='Z')?(c-'A')
+                :(c>='a'&&c<='z')?(c-'a'+26)
+                :(c>='0'&&c<='9')?(c-'0'+52)
+                :(c=='+')?62
+                :(c=='/')?63
+                :(c=='=')?64
+                :0xff;
+        }
+    }
+}
+
+#endif
